e1840d1253222cca6066f47fe330d3a36e18d119
LLEFEVRE <loic.lefevre@gmail.com>
2023-06-12 11:32:41 +0200

    fix bug _id

--- src/main/java/com/oracle/mongo2ora/Main.java
+++ src/main/java/com/oracle/mongo2ora/Main.java
@@ -104,7 +104,7 @@ import static java.util.stream.Collectors.toList;
  * - help migration using properties file for per collection configuration (range partitioning, SODA collection columns, types etc..., filtering...)
  */
 public class Main {
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 
 	private static final Logger LOGGER = Loggers.getLogger("main");
 
